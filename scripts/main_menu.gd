c542cfd25bf93313dba1178a2113b2b7f10f3286
Onni Elonen <accounts+github@eloneon.com>
2026-01-30 20:21:23 +0200

    new scenes

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,9 +1,14 @@
 extends MarginContainer
 
 @onready var start_scene_btn: Button = %StartSceneBtn
+@onready var start_game_btn: Button = %StartGameBtn
 
 func _ready() -> void:
     start_scene_btn.pressed.connect(_on_start_scene_btn_pressed)
+    start_game_btn.pressed.connect(_on_start_game_btn_pressed)
 
 func _on_start_scene_btn_pressed() -> void:
     get_tree().change_scene_to_file("res://scenes/test_scene.tscn")
+
+func _on_start_game_btn_pressed() -> void:
+    get_tree().change_scene_to_file("res://scenes/safehouse_scene.tscn")
