69b48af447f574de369b2418c5db5d844d0c6dc5
Onni Elonen <accounts+github@eloneon.com>
2026-01-31 23:32:53 +0200

    ui stuff

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -1,18 +1,25 @@
 extends CanvasLayer
 
+@onready var battery_rect: TextureRect = %BatteryTexture
 @onready var battery_label: Label = %BatteryLabel
 @onready var quota_label: Label = %QuotaLabel
 @onready var sanity_label: Label = %SanityLabel
+@onready var coin_label: Label = %CoinLabel
 
 func _process(_delta: float) -> void:
-	var player := get_tree().get_first_node_in_group("player") as Player
+	var player = get_tree().get_first_node_in_group("player") as Player
+
 	if player != null:
-		battery_label.text = "Battery: %d/%d (%d spare)" % [
-			player.battery_charge,
-			player.battery_max_charge,
-			GameManager.get_item_count(Enums.Item.BATTERY) if GameManager else 0
-		]
-		sanity_label.text = "Sanity: %d" % player.sanity
+		if player.battery_charge < Constants.BATTERY_MAX_CHARGE * 0.25:
+			battery_rect.texture = preload("res://assets/battery/battery1.tres")
+		elif player.battery_charge < Constants.BATTERY_MAX_CHARGE * 0.5:
+			battery_rect.texture = preload("res://assets/battery/battery2.tres")
+		elif player.battery_charge < Constants.BATTERY_MAX_CHARGE * 0.75:
+			battery_rect.texture = preload("res://assets/battery/battery3.tres")
+		else:
+			battery_rect.texture = preload("res://assets/battery/battery4.tres")
 
 	if GameManager != null:
-		quota_label.text = "Quota: %d/%d" % [GameManager.ghosts_captured_this_level, GameManager.quota_this_level]
+		battery_label.text = str(GameManager.get_item_count(Enums.Item.BATTERY))
+		quota_label.text = "%d/%d" % [GameManager.ghosts_captured_this_level, GameManager.quota_this_level]
+		coin_label.text = str(GameManager.get_money())
