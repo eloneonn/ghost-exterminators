2ba547e99ee786a48759bf59528373f79b1a3554
vihtorialves <vitor.fines@gmail.com>
2026-01-31 20:14:09 -0300

    GunShooting

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -4,11 +4,11 @@ extends MarginContainer
 @onready var start_game_btn: Button = %StartGameBtn
 
 func _ready() -> void:
-    start_scene_btn.pressed.connect(_on_start_scene_btn_pressed)
-    start_game_btn.pressed.connect(_on_start_game_btn_pressed)
+	start_scene_btn.pressed.connect(_on_start_scene_btn_pressed)
+	start_game_btn.pressed.connect(_on_start_game_btn_pressed)
 
 func _on_start_scene_btn_pressed() -> void:
-    get_tree().change_scene_to_file("res://scenes/test_scene.tscn")
+	get_tree().change_scene_to_file("res://scenes/test_scene.tscn")
 
 func _on_start_game_btn_pressed() -> void:
-    get_tree().change_scene_to_file("res://scenes/safehouse_scene.tscn")
+	get_tree().change_scene_to_file("res://scenes/safehouse_scene.tscn")
